b0f0ed410d8f704d39527533457764ece7c25605
Vinay Prajapati <vinay.prajapati@iongroup.com>
2019-07-14 23:39:27 -0700

    changed the app name

--- function-app/src/components/Main.tsx
+++ function-app/src/components/Main.tsx
@@ -6,14 +6,12 @@ import TaskDashboard from './dashboard/TaskDashboard';
 import Footer from './footer/Footer';
 import './Main.scss';
 
-function Main() {
-  return (
-    <Box component={"div"} className="main">
-      <Header/>
-      <TaskDashboard/>
-      <Footer/>
-    </Box>
-  );
-}
+const Main = () => (
+  <Box component={"div"} className="main">
+    <Header/>
+    <TaskDashboard/>
+    <Footer/>
+  </Box>
+);
 
 export default Main;

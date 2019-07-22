edde5515d03896ca8ad053cb3638dece6fa203bc
Vinay Prajapati <vinay.prajapati@iongroup.com>
2019-07-22 06:22:19 -0700

    new component

---
+++ function-app/src/components/dashboard/newTaskRow/NewTaskRow.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import { TextField } from '@material-ui/core';
+
+import './NewTaskRow.scss';
+import { Priority, Task, TaskStatus } from '../../types/mapping';
+import { KEY_CODES } from '../../../commons/utils/keycodes';
+import { restPost } from '../../../commons/utils/RestRequest';
+import { BACKEND_URLS } from '../../../commons/constants';
+
+const NewTaskRow = (props) => {
+  const taskInputElementRef = React.useRef(null);
+  const { onAddNewTask } = props;
+
+  useEffect(() => {
+    taskInputElementRef.current.focus();
+  }, []);
+
+  const saveTask = (e) => {
+    if (KEY_CODES.ENTER == e.keyCode || KEY_CODES.ENTER == e.which) {
+      const value: string = e.target.value;
+
+      const task: Task = {
+        title: value,
+        description: '',
+        subtasks: [],
+        priority: Priority.MEDIUM,
+        currentStatus: TaskStatus.DEFINED,
+        date: new Date()
+      }
+
+      restPost(BACKEND_URLS.tasks, task)
+        .then((response) => {
+          taskInputElementRef.current.value = '';
+          onAddNewTask(response.data.data);
+        })
+        .catch(err => {
+          console.error(`error while trying to save task ${task} : ${err}`)
+        });
+    }
+  }
+
+  return (
+    <TextField
+      className="inputBoxNewTask"
+      label="Add new Task"
+      placeholder="Please add task details and press enter key"
+      variant="filled"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      inputRef={taskInputElementRef}
+      onKeyPress={saveTask}
+    />
+  );
+}
+
+export default NewTaskRow;
